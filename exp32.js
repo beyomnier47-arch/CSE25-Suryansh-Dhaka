0200c43dce8bf23d7b6cc49828c4404e239466f0
suryansh dhaka <beyomnier47@gmail.com>
2026-06-25 00:36:16 +0530

    Update

---
+++ exp32.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    // p</meta></meta>
+</head>
+<body>
+    // commnt
+    <div class="flex justify-center items-center">
+    <img src="https://images.pexels.com/photos/35252922/pexels-photo-35252922.jpeg" class="h-32 w-32 rounded-full"></div>
+    <button class="border rounded-full border-red-900  w-16 h-16">Click Me</button>
+   <button class="border rounded-3xl border-red-900" >Click Me</button>
+   // style
+    <button class="border rounded border-red-900" >Click Me</button>
+</body>
+// style</meta>
+// meta</head>
+// head
+</html>
